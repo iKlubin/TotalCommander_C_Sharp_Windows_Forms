e592f9f620ddca9bb5e0e339cc3c7025ffd5bf50
Klubin <clubin2004@gmail.com>
2023-12-12 23:04:08 +0200

    main

--- TotalCommander/Form1.cs
+++ TotalCommander/Form1.cs
@@ -1,33 +1,15 @@
 ﻿using Microsoft.VisualBasic;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlTypes;
-using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TotalCommander
 {
     public partial class Form1 : Form
     {
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private static HookProc proc = HookCallback;
-        private static IntPtr hook = IntPtr.Zero;
         public Form1()
         {
             InitializeComponent();
-            hook = SetHook(proc);
-            Application.Run();
-            UnhookWindowsHookEx(hook);
 
             textBox1.Text = "C:\\";
             textBox2.Text = "C:\\Users";
@@ -171,35 +153,6 @@ namespace TotalCommander
                 MessageBox.Show("Указанный путь не существует.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        private static IntPtr SetHook(HookProc proc)
-        {
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
-            {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
-            }
-        }
-        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
-        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if ((nCode >= 0) && (wParam == (IntPtr)WM_KEYDOWN))
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-                if (((Keys)vkCode == Keys.Enter))
-                {
-                    return (IntPtr)1;
-                }
-            }
-            return CallNextHookEx(hook, nCode, wParam, lParam);
-        }
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -219,17 +172,14 @@ namespace TotalCommander
         {
             if (Directory.Exists(textBox1.Text))
             {
-                // Отображение окна ввода имени папки
                 string folderName = Interaction.InputBox("Введите имя новой папки:", "Создание папки", "");
 
                 if (!string.IsNullOrEmpty(folderName))
                 {
-                    // Создание полного пути к новой папке
                     string newFolderPath = Path.Combine(textBox1.Text, folderName);
 
                     try
                     {
-                        // Попытка создать новую папку
                         Directory.CreateDirectory(newFolderPath);
 
                         MessageBox.Show($"Папка '{folderName}' успешно создана.", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -238,8 +188,6 @@ namespace TotalCommander
                     {
                         MessageBox.Show($"Ошибка при создании папки: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
-
-                    // Обновление содержимого DataGridView после создания новой папки
                     loadFolder1();
                 }
             }
@@ -307,17 +255,14 @@ namespace TotalCommander
         {
             if (Directory.Exists(textBox2.Text))
             {
-                // Отображение окна ввода имени папки
                 string folderName = Interaction.InputBox("Введите имя новой папки:", "Создание папки", "");
 
                 if (!string.IsNullOrEmpty(folderName))
                 {
-                    // Создание полного пути к новой папке
                     string newFolderPath = Path.Combine(textBox2.Text, folderName);
 
                     try
                     {
-                        // Попытка создать новую папку
                         Directory.CreateDirectory(newFolderPath);
 
                         MessageBox.Show($"Папка '{folderName}' успешно создана.", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -326,8 +271,6 @@ namespace TotalCommander
                     {
                         MessageBox.Show($"Ошибка при создании папки: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
-
-                    // Обновление содержимого DataGridView после создания новой папки
                     loadFolder2();
                 }
             }
@@ -335,47 +278,33 @@ namespace TotalCommander
 
         private void button5_Click(object sender, EventArgs e)
         {
-            // Проверяем, есть ли выделенные строки
             if (dataGridView1.SelectedRows.Count > 0)
             {
-                // Получаем тип элемента (Файл или Папка)
                 string itemType = dataGridView1.SelectedRows[0].Cells["ItemType"].Value.ToString();
-                // Получаем имя элемента
                 string itemName = dataGridView1.SelectedRows[0].Cells["ItemName"].Value.ToString();
-                // Получаем полный путь к элементу
                 string itemPath = Path.Combine(textBox1.Text, itemName);
-
-                // Проверяем, что элемент существует
                 if ((itemType == "Файл" && File.Exists(itemPath)) || (itemType == "Папка" && Directory.Exists(itemPath)))
                 {
                     try
                     {
                         if (itemType == "Файл")
                         {
-                            // Копируем файл
-                            File.Copy(itemPath, Path.Combine(textBox2.Text, itemName), true); // true - перезаписывать, если файл уже существует
+                            File.Copy(itemPath, Path.Combine(textBox2.Text, itemName), true);
                            
                         }
                         else if (itemType == "Папка")
                         {
-                            // Копируем папку и её содержимое
                             CopyDirectory(itemPath, Path.Combine(textBox1.Text, itemName));
                         }
                         loadFolder2();
                         foreach (DataGridViewRow row in dataGridView2.Rows)
                         {
-                            // Получаем значение из ячейки "ItemName"
                             string rowItemName = row.Cells["ItemName"].Value.ToString();
 
                             if (rowItemName.Equals(itemName, StringComparison.OrdinalIgnoreCase))
                             {
-                                // Выделяем строку
                                 row.Selected = true;
-
-                                // Прокручиваем к выделенной строке (если она не видна)
                                 dataGridView1.FirstDisplayedScrollingRowIndex = row.Index;
-
-                                // Выход из цикла после первого совпадения
                                 break;
                             }
                         }
@@ -390,20 +319,15 @@ namespace TotalCommander
         }
         private void CopyDirectory(string sourcePath, string destinationPath)
         {
-            // Создаем папку, если её нет
             if (!Directory.Exists(destinationPath))
             {
                 Directory.CreateDirectory(destinationPath);
             }
-
-            // Копируем файлы
             foreach (string file in Directory.GetFiles(sourcePath))
             {
                 string destFile = Path.Combine(destinationPath, Path.GetFileName(file));
                 File.Copy(file, destFile, true);
             }
-
-            // Рекурсивно копируем подпапки
             foreach (string subDirectory in Directory.GetDirectories(sourcePath))
             {
                 string destSubDirectory = Path.Combine(destinationPath, Path.GetFileName(subDirectory));
